9bd1b860d3f2298d55b8659ba756d61898f4e13a
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-03-10 10:29:01 +0000

    Sintaxe Switch por padrões

---
+++ GrandpaPatternSwitch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GrandpaPatternSwitch
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Variáveis auxiliares
+            string question;
+
+            // Colocar questão ao utilizador
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            // Verificar se se trata de uma questão conhecida e determinar uma
+            // resposta adequada
+            string response=question switch
+            {
+                "How are you?" => "I'm fine, thank you",
+                "What's your name?" => "Gran'pa",
+                "What's your mission?" => "Destroy mankind!",
+                _ => "You got me, I'm not THAT smart!",
+            };
+
+            // Responder adequadamente
+            Console.WriteLine(response);
+        }
+    }
+}
