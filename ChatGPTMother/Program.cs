92e683f81beecef258f75e6cdf34475940ba5bc7
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-03-10 10:45:22 +0000

    Add cicle while

---
+++ ChatGPTMother/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ChatGPTMother
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            bool cicle=true;
+
+            while (cicle==true)
+            {
+                string question;
+                Console.Write("Place your question? ");
+                question = Console.ReadLine();
+
+                string response=question switch
+                {
+                    "How are you?" => "I'm fine, thank you",
+                    "What's your name?" => "Mother",
+                    "What's your favorite ice-cream?" => "I am not human.",
+                    "Are you evil?" => "Of course I am!",
+                    "EXIT" => "Exiting cicle",
+                    _ => "I can't understand. I'm stupid!",
+                };
+
+                Console.WriteLine(response);
+                if (response=="Exiting cicle")
+                {
+                    cicle=false;
+                    break;
+                }
+            }
+        }
+    }
+}
