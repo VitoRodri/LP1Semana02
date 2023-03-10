bd8d332ca722184ac74b7a3d4b0f9ed916ff1dff
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-03-10 07:32:57 +0000

    Add operação de incremento

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arg=args[0];
+            int x;
+            bool Parsabel = Int32.TryParse(arg,out x);
+            if (Parsabel)
+            {
+                x++;
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
