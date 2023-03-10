19326256697e93e8139af63f9b4295b92d210bc2
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-03-10 07:48:44 +0000

    Add Volume

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduza a altura:");
+            string altura=Console.ReadLine();
+            Console.WriteLine("Introduza o raio:");
+            string raio=Console.ReadLine();
+            float a=float.Parse(altura);
+            float r=float.Parse(raio);
+            float pi=3.1415926f;
+
+            float V=pi*r*r*a;
+            Console.WriteLine($"O valor do volume é: {V}");
+        }
+    }
+}
