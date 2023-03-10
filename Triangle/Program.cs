ba4c4ba9d4d3c93157268a8ccce8a214b86e4ba9
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-03-10 11:08:05 +0000

    Add Triangle

---
+++ Triangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=Int32.Parse(args[0]);
+            char a=char.Parse(args[1]);
+            
+
+            for (int i=1; i<=x;i++)
+            {
+                string chara = new String(a,i);
+                Console.WriteLine(chara);
+
+            }
+        }
+    }
+}
